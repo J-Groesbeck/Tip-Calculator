3c39a60ef998fe277576a4eb2ccc4e84e05da854
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-01-23 14:29:37 -0500

    FInished JS

---
+++ script.js
@@ -0,0 +1,38 @@
+function calculateAndDisplay() {
+    // Get total bill and number of people - return an error if not a number
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    if(isNaN(bill)) {
+        alert('Error: Please enter a valid bill amount- only numbers')
+        return
+    }
+    let numOfPpl = parseInt(document.getElementById('numOfPpl').value)
+    if(isNaN(numOfPpl)) {
+        alert('Error: Please enter a valid number of people- only numbers')
+        return
+    }
+
+    // Recognize how much they want to tip
+    let service = document.getElementById('serviceQuality').value
+    let tipPercent = 0
+    if(service === 'Great') {
+        tipPercent = 0.2
+    } else if(service === 'Good') {
+        tipPercent = 0.15
+    } else if(service === 'Poor') {
+        tipPercent = 0.1
+    } else if(service === 'Default') {
+        alert('Error: Please enter a valid service quality')
+        return
+    }
+
+    // Calculate how much they should tip
+    let tipTotal = tipPercent * bill
+    // Calculate total bill
+    let totalBill = tipTotal + bill
+    // Calculate amount per person
+    let amountPerPerson = totalBill / numOfPpl
+    // Display these numbers
+    document.getElementById('tipResult').innerText = 'Tip: $' + tipTotal.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amountPerPerson.toFixed(2)
+}
